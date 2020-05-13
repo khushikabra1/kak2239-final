79a968332f1bcc0b1460384fab3432ea37e31a19
Khushi Kabra <khushikabra@Khushis-MacBook-Pro.local>
2020-05-13 16:48:00 +0530

    Completed Home Page and created Assignments and Classes Pages

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ app = Flask(__name__)
 def engi1006():
     return render_template("index.html")
 
+@app.route("/assignments")
+def assignments():
+    return render_template("assignments.html")
+
+@app.route("/classes")
+def classes():
+    return render_template("classes.html")
+
 #start the server
 if __name__ == "__main__":
     app.run()
