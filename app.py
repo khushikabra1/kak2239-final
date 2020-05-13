0ecb3c2bba3cc08b5514b2ed4b0e8328d9f09c54
Khushi Kabra <khushikabra@Khushis-MacBook-Pro.local>
2020-05-13 16:31:13 +0530

    Working on Homepage

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 14:57:17 2020
-
-@author: etill
-"""
 
 #import statements
 from flask import Flask, render_template
@@ -13,9 +7,10 @@ app = Flask(__name__)
 
 #static route
 @app.route("/")
-def hello():
+def engi1006():
     return render_template("index.html")
 
 #start the server
 if __name__ == "__main__":
     app.run()
+    
